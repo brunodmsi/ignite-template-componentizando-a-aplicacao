6892fe6c8ccd2e9b7bf27b967de1e6cae0320edf
Bruno De Masi <bruno.demasi@terras.agr.br>
2021-05-20 01:43:43 -0300

    Finalizando alterações

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,37 @@
-export function Content() {
-  // Complete aqui
+import React from 'react';
+import { Button } from '../components/Button';
+import { MovieCard } from '../components/MovieCard';
+
+import '../styles/sidebar.scss';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Runtime: string;
+  Ratings: Array<{
+    Value: string;
+  }>;
 }
+
+interface ContentProps {
+  movies: MovieProps[];
+}
+
+export const Content: React.FC<ContentProps> = ({ movies }) => {
+  return (
+    <main>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <MovieCard 
+            key={movie.imdbID}
+            title={movie.Title} 
+            poster={movie.Poster} 
+            runtime={movie.Runtime} 
+            rating={movie.Ratings[0].Value} 
+          />
+        ))}
+      </div>
+    </main>
+  )
+} 
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
-  // Complete aqui
+import React from 'react';
+import { Button } from '../components/Button';
+import '../styles/sidebar.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'drama' | 'horror' | 'family' | 'documentary';
+  title: string;
 }
+
+interface SidebarProps {
+  genres: GenreResponseProps[];
+  handleClickButton: (id: number) => void;
+  selectedGenreId: number;
+}
+
+export const SideBar: React.FC<SidebarProps> = ({ genres, handleClickButton, selectedGenreId }) => {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
+} 
